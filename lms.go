1ee90163129b333d31f436b671d3649eac65c092
hallazzang <hallazzang@gmail.com>
2017-11-09 02:10:28 +0900

    알림 가져오기 구현(GetNotificationsByPage)

--- lms.go
+++ lms.go
@@ -8,10 +8,31 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"regexp"
+	"strconv"
 	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-// Client is a wrapper for a single http.Client instance
+// Notification represents a single notification.
+type Notification struct {
+	Type           string
+	Title          string
+	Datetime       string
+	Submitted      bool
+	Lecture        string
+	Professor      string
+	PreviewContent string
+}
+
+// String returns a string representation of a notification
+// in form of {Type: Title}.
+func (n *Notification) String() string {
+	return fmt.Sprintf("{%s: %s}", n.Type, n.Title)
+}
+
+// Client is a wrapper for a single http.Client instance.
 type Client struct {
 	httpClient *http.Client
 }
@@ -58,7 +79,6 @@ func (c *Client) Login(id, pw string) (bool, error) {
 		"usr_id":    {id},
 		"usr_pwd":   {pw},
 	}
-
 	resp, err := c.httpClient.PostForm(target, params)
 	if err != nil {
 		return false, err
@@ -84,4 +104,85 @@ func (c *Client) MustLogin(id, pw string) bool {
 		panic(err)
 	}
 	return result
+}
+
+// GetNotificationsByPage returns a slice of notifications for given page.
+// The default page size(notifications per page) is 20.
+// If you want to change the page size, you can use GetNotifications.
+func (c *Client) GetNotificationsByPage(page int) ([]*Notification, error) {
+	target := "http://lms.pknu.ac.kr/ilos/mp/mypage_main_list.acl"
+	params := url.Values{
+		"start":    {strconv.Itoa((page-1)*20 + 1)},
+		"display":  {strconv.Itoa(20)},
+		"GUBUN":    {""},
+		"encoding": {"utf-8"},
+	}
+	resp, err := c.httpClient.PostForm(target, params)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		return nil, err
+	}
+
+	nts := make([]*Notification, 20)
+
+	dateRe := regexp.MustCompile(`^(.+?) \| 마감일\((.+?)\)$`)
+
+	doc.Find(".resultBox li:nth-of-type(2)").Each(func(i int, s *goquery.Selection) {
+		var typeText, title, date, lecture, professor, previewContent string
+		var submitted bool
+
+		{
+			t := strings.SplitN(strings.TrimSpace(s.Find(".site-link").First().Text()), ": ", 2)
+			typeText, title = t[0], t[1]
+		}
+
+		{
+			t := s.Find("span").Map(func(i int, s *goquery.Selection) string {
+				return strings.TrimSpace(s.Text())
+			})
+			previewContent = t[1]
+			if typeText == "과제" {
+				matches := dateRe.FindStringSubmatch(t[0])
+				submitted = matches[1] == "제출"
+				date = matches[2]
+			} else {
+				submitted = false
+				date = t[0]
+			}
+		}
+
+		{
+			t := s.Find("div").Last().Find("a").Map(func(i int, s *goquery.Selection) string {
+				return strings.TrimSpace(s.Text())
+			})
+			lecture, professor = t[1], t[0]
+		}
+
+		nts[i] = &Notification{
+			Type:           typeText,
+			Title:          title,
+			Datetime:       date,
+			Submitted:      submitted,
+			Lecture:        lecture,
+			Professor:      professor,
+			PreviewContent: previewContent,
+		}
+	})
+
+	return nts, nil
+}
+
+// MustGetNotificationsByPage returns a slice of notifications for given page,
+// panics when an error has occurred.
+func (c *Client) MustGetNotificationsByPage(page int) []*Notification {
+	nts, err := c.GetNotificationsByPage(page)
+	if err != nil {
+		panic(err)
+	}
+	return nts
 }
