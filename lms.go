521dd37cc782a0f4865571b8fd94fbe9159ee17e
hallazzang <hallazzang@gmail.com>
2017-11-11 23:58:26 +0900

    GetNotifications 메소드 추가, Notification 구조체 수정
    - GetNotificationsByPage는 이제 GetNotifications를 호출
    - Notification 구조체는 Lecture 구조체를 내부에 포함, Lecture 구조체는
    - 강의의 키(KJKEY)와 이름을 가지고 있음
    - Must* 함수 형태 변경

--- lms.go
+++ lms.go
@@ -3,6 +3,7 @@ package pknulms
 
 import (
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -15,13 +16,23 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+// Lecture represents a single lecture.
+type Lecture struct {
+	Key  string
+	Name string
+}
+
 // Notification represents a single notification.
+// Datetime field might hold different datetime format for different type of notifications.
+// Submitted field holds a boolean value whether the assignment has assigned or not
+// if the Type of notification is assignment(in string form, "과제") else always false.
 type Notification struct {
+	Link           string
 	Type           string
 	Title          string
 	Datetime       string
 	Submitted      bool
-	Lecture        string
+	Lecture        *Lecture
 	Professor      string
 	PreviewContent string
 }
@@ -62,11 +73,11 @@ func NewClient() (*Client, error) {
 
 // MustNewClient attempts to create a new client, panics when an error has occurred.
 func MustNewClient() *Client {
-	c, err := NewClient()
-	if err != nil {
+	if c, err := NewClient(); err != nil {
 		panic(err)
+	} else {
+		return c
 	}
-	return c
 }
 
 // Login logs client into LMS.
@@ -99,21 +110,20 @@ func (c *Client) Login(id, pw string) (bool, error) {
 
 // MustLogin attempts to login, panics when an error has occurred.
 func (c *Client) MustLogin(id, pw string) bool {
-	result, err := c.Login(id, pw)
-	if err != nil {
+	if result, err := c.Login(id, pw); err != nil {
 		panic(err)
+	} else {
+		return result
 	}
-	return result
 }
 
-// GetNotificationsByPage returns a slice of notifications for given page.
-// The default page size(notifications per page) is 20.
-// If you want to change the page size, you can use GetNotifications.
-func (c *Client) GetNotificationsByPage(page int) ([]*Notification, error) {
+// GetNotifications returns a slice of notifications for given start offset and count.
+// Note that start offset begins from 1 so the FIRST notification would be at offset 1, not 0.
+func (c *Client) GetNotifications(start, count int) (result []*Notification, e error) {
 	target := "http://lms.pknu.ac.kr/ilos/mp/mypage_main_list.acl"
 	params := url.Values{
-		"start":    {strconv.Itoa((page-1)*20 + 1)},
-		"display":  {strconv.Itoa(20)},
+		"start":    {strconv.Itoa(start)},
+		"display":  {strconv.Itoa(count)},
 		"GUBUN":    {""},
 		"encoding": {"utf-8"},
 	}
@@ -128,61 +138,95 @@ func (c *Client) GetNotificationsByPage(page int) ([]*Notification, error) {
 		return nil, err
 	}
 
-	nts := make([]*Notification, 20)
-
 	dateRe := regexp.MustCompile(`^(.+?) \| 마감일\((.+?)\)$`)
+	jsStringRe := regexp.MustCompile(`'(.*?)'`)
 
-	doc.Find(".resultBox li:nth-of-type(2)").Each(func(i int, s *goquery.Selection) {
-		var typeText, title, date, lecture, professor, previewContent string
+	doc.Find(".resultBox li:nth-of-type(2)").EachWithBreak(func(i int, s *goquery.Selection) bool {
+		var link, typeText, title, date, professor, previewContent string
 		var submitted bool
+		var lecture Lecture
 
-		{
-			t := strings.SplitN(strings.TrimSpace(s.Find(".site-link").First().Text()), ": ", 2)
-			typeText, title = t[0], t[1]
+		a := s.Find(".site-link").First()
+		typeText, title = splitString(strings.TrimSpace(a.Text()), ": ")
+
+		if href, exists := a.Attr("href"); !exists {
+			e = fmt.Errorf("Missing 'href' attribute for the site-link tag.")
+			return false
+		} else {
+			link = "http://lms.pknu.ac.kr" + href
 		}
 
-		{
-			t := s.Find("span").Map(func(i int, s *goquery.Selection) string {
-				return strings.TrimSpace(s.Text())
-			})
-			previewContent = t[1]
-			if typeText == "과제" {
-				matches := dateRe.FindStringSubmatch(t[0])
-				submitted = matches[1] == "제출"
-				date = matches[2]
-			} else {
-				submitted = false
-				date = t[0]
+		if onclick, exists := a.Attr("onclick"); !exists {
+			e = errors.New("Missing 'onclick' attribute for the site-link tag.")
+			return false
+		} else {
+			matches := jsStringRe.FindAllStringSubmatch(onclick, -1)
+			lecture.Key = matches[1][1]
+		}
+
+		t := s.Find("span").Map(func(i int, s *goquery.Selection) string {
+			return strings.TrimSpace(s.Text())
+		})
+		previewContent = t[1]
+		if typeText == "과제" {
+			matches := dateRe.FindStringSubmatch(t[0])
+			submitted = matches[1] == "제출"
+			date = matches[2]
+		} else {
+			submitted = false
+			date = t[0]
+		}
+
+		s.Find("div").Last().Find("a").Each(func(i int, s *goquery.Selection) {
+			text := strings.TrimSpace(s.Text())
+			if i == 0 {
+				professor = text
+			} else if i == 1 {
+				lecture.Name = text
 			}
-		}
+		})
 
-		{
-			t := s.Find("div").Last().Find("a").Map(func(i int, s *goquery.Selection) string {
-				return strings.TrimSpace(s.Text())
-			})
-			lecture, professor = t[1], t[0]
-		}
-
-		nts[i] = &Notification{
+		result = append(result, &Notification{
+			Link:           link,
 			Type:           typeText,
 			Title:          title,
 			Datetime:       date,
 			Submitted:      submitted,
-			Lecture:        lecture,
+			Lecture:        &lecture,
 			Professor:      professor,
 			PreviewContent: previewContent,
-		}
+		})
+
+		return true
 	})
 
-	return nts, nil
+	return
+}
+
+// MustGetNotifications returns a slice of notifications for given start offset and count,
+// panics when an error has occurred.
+// Note that start offset begins from 1 so the FIRST notification would be at offset 1, not 0.
+func (c *Client) MustGetNotifications(start, count int) []*Notification {
+	if result, err := c.GetNotifications(start, count); err != nil {
+		panic(err)
+	} else {
+		return result
+	}
+}
+
+// GetNotificationsByPage returns a slice of notifications for given page.
+// The default page size(notifications per page) is 20.
+// If you want to change the page size, you can use GetNotifications.
+func (c *Client) GetNotificationsByPage(page int) (result []*Notification, e error) {
+	return c.GetNotifications((page-1)*20+1, 20)
 }
 
 // MustGetNotificationsByPage returns a slice of notifications for given page,
 // panics when an error has occurred.
 func (c *Client) MustGetNotificationsByPage(page int) []*Notification {
-	nts, err := c.GetNotificationsByPage(page)
-	if err != nil {
+	if result, err := c.GetNotificationsByPage(page); err != nil {
 		panic(err)
+	} else {
+		return result
 	}
-	return nts
 }
